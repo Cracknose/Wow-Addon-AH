2ce6b6f8a97903579bcb4d3085a2428706b59970
Cracknose <lord.exeon@gmail.com>
2018-11-19 23:05:37 +0100

    Folder Cleaning

--- Workspace/Register-Event.lua
+++
@@ -1,9 +0,0 @@
-
-frameName = CreateFrame("Frame")
-
-frameName:RegisterEvent("AUCTION_ITEM_LIST_UPDATE")
-frameName:SetScript("OnEvent", EventHandler)
-
-function EventHandler(e)
-	-- Do Shiet!!
-end
